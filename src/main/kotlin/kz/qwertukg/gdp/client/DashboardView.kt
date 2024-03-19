ea8e7c490ba66e42f88eb1178dec92d89c2f4d70
daniil <daniil.rakhmatulin@gmail.com>
2024-03-19 09:44:57 +0500

    Add contributor dashboard view

---
+++ src/main/kotlin/kz/qwertukg/gdp/client/DashboardView.kt
@@ -0,0 +1,13 @@
+package kz.qwertukg.gdp.client
+
+import kotlinx.html.DIV
+
+fun DIV.renderContributorDashboard(
+    projectId: Int,
+    events: List<String>,
+    users: List<String>,
+    selectedEvents: List<String>,
+    selectedUsers: List<String>
+) {
+
+}
