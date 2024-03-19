120e5cb501b86fa660e6268e96f42767c7d42912
daniil <daniil.rakhmatulin@gmail.com>
2024-03-19 10:01:05 +0500

    test env vars

--- src/main/kotlin/kz/qwertukg/plugins/JsonService.kt
+++ src/main/kotlin/kz/qwertukg/plugins/JsonService.kt
@@ -1,16 +1,13 @@
 package kz.qwertukg.plugins
 
-import io.ktor.server.application.*
 import io.ktor.server.config.*
 import java.io.File
 
 class JsonService(private val config: ApplicationConfig) {
     val defaultLang = "en"
+    private val path = config.property("app.dataPath").getString()
 
     fun getDataByLang(lang: String): String {
-        val path = config.propertyOrNull("app.dataPath")?.getString()
-            ?: throw NullPointerException("Data source not found")
-
         return File("$path/$lang/cv.json").readText()
     }
 }
