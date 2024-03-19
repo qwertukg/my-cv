850e9318f8ff7dc0cddbfd7d0eb5b5c15b228a19
daniil <daniil.rakhmatulin@gmail.com>
2024-03-19 10:09:32 +0500

    clean up code

--- src/main/resources/static/js/gdp.js
+++ src/main/resources/static/js/gdp.js
@@ -1,75 +1,33 @@
 const ctx = document.getElementById('myChart');
 
-
-
-
-
-// // Получаем текущий URL
-// const currentUrl = window.location.href;
-//
-// // Находим последний слеш в URL и получаем подстроку после него
-// const lastSlashIndex = currentUrl.lastIndexOf('/');
-// const pathAfterLastSlash = currentUrl.substring(lastSlashIndex + 1);
-
-
-
-// Получаем текущий URL
-const currentUrl = window.location.href;
-
-const parts = currentUrl.split('/');
+const parts = window.location.href.split('/');
 const projectId = parts[parts.length - 2];
 const after = parts[parts.length - 1];
 
-// Проверяем, существует ли путь после последнего слеша
 if (projectId && after) {
-    // Выполняем GET-запрос к полученному пути
-    fetch("/e/" + projectId + "/" + after)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            const config = {
-                type: 'radar',
-                data: data,
-                options: {
-                    elements: {
-                        line: {
-                            borderWidth: 3
-                        }
+    fetch("/e/" + projectId + "/" + after).then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    }).then(data => {
+        const config = {
+            type: 'radar',
+            data: data,
+            options: {
+                elements: {
+                    line: {
+                        borderWidth: 1
                     }
-                },
-            };
+                }
+            },
+        };
 
-            // render
-            new Chart(ctx, config);
-        })
-        .catch(error => {
-            console.error('Ошибка при выполнении запроса:', error);
-        });
+        // render
+        new Chart(ctx, config);
+    }).catch(error => {
+        console.error('Ошибка при выполнении запроса:', error);
+    });
 } else {
     console.log('После последнего слеша в URL ничего не найдено.');
 }
-
-
-// const data = {
-//     labels: [
-//         'Eating',
-//         'Drinking',
-//         'Sleeping',
-//         'Designing',
-//         'Coding',
-//         'Cycling',
-//         'Running'
-//     ],
-//     datasets: [{
-//         label: 'My First Dataset',
-//         data: [65, 59, 90, 81, 56, 55, 40]
-//     }, {
-//         label: 'My Second Dataset',
-//         data: [28, 48, 40, 19, 96, 27, 100]
-//     }]
-// };
-
